88f56b944bf8305c9fc4eaa86bb25af9a43b28b5
sotela88 <j-ms-27@hotmail.com>
2020-03-13 15:49:57 +0100

    youtube

--- index.js
+++ index.js
@@ -11,10 +11,11 @@
 
 
 //////// main function --menu and "videos" injection--///////
-function pic() {
-  let video = document.getElementById("right");
+let video = document.getElementById("right");
   let left_menu = document.getElementById("left");
   let inner_nav = document.getElementById("inner_nav");
+function pic() {
+  
 
 
 ///////////// lateral manu //////
@@ -26,11 +27,11 @@ function pic() {
     <a href="#"><i class="fas fa-folder-open"></i>Subscriptions</a>
     <span class="line"></span>
 
-    <h3>SUBSCRIPTIONS</h3>`;
+    `;
   }
 
   //////// inner top menu  ////////
-  inner_nav.innerHTML = `<ul id="menu">
+  inner_nav.innerHTML = `<ul id="menu" class="menu active">
   <li>HOME</li>
   <li>VIDEOS</li>
   <li>PLAYLIST</li>
@@ -48,7 +49,7 @@ function pic() {
     video_inner = document.innerHTML = ` <div class="img_after"><img class="after"
                     src="https://picsum.photos/350/200?random=${Math.floor(
                       Math.random() * 9
-                    )}" alt=""></a></div>
+                    )}" alt=""onClick="zoom()" ></a></div>
         <h5 class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, dolorum.</h5>
         <p><span class="mill" id="minute">356184 mill </span><span class="view">3 Years ago</span></p>
     </div>`;
@@ -64,8 +65,16 @@ function color1() {
   document.getElementById("nav").style.background = "rgba(136, 132, 203)";
   document.getElementById("left").style.background = "rgba(136, 132, 203)";
   document.getElementById("menu").style.background = "rgba(136, 132, 203)";
-  document.body.setAttribute( 'style', 'color;black ;!important' );
+  
+  for (let i = 0; i < video.getElementsByTagName("h5").length; i++) {
+    video.getElementsByTagName("h5")[i].style.color='black' ; 
+  }
+
+  for (let i = 0; i < video.getElementsByTagName("span").length; i++) {
+    video.getElementsByTagName("span")[i].style.color='black' ;
+  }
 }
+
 
 function color2() {
   document.getElementById("right").style.background = "rgb(23, 41, 57)";
@@ -73,3 +82,34 @@ function color2() {
   document.getElementById("left").style.background = "rgb(23, 41, 57)";
   document.getElementById("menu").style.background = "rgb(23, 41, 57)";
 }
+
+// function zoom(){
+//   for (let i = 0; i < video.getElementsByTagName("img").length; i++) {
+//     video.getElementsByTagName("img")[1].setAttribute(
+//   "style", "border: 2px solid red ");
+    
+//   }
+ 
+ 
+// }
+const contenedor_video = document.getElementById("toggle");
+const contenedor_menu = document.getElementById("menu");
+
+document.getElementById('boton-menu').addEventListener('click', () => {
+  contenedor_video.classList.toggle('active');
+  contenedor_menu.classList.toggle('active');
+});
+
+const comprobarAncho = () => {
+	if(window.innerWidth >= 768){
+		contenedor_video.classList.remove('active');
+	} else {
+		contenedor_video.classList.add('active');
+	}
+}
+
+comprobarAncho();
+
+window.addEventListener('resize', () => {
+	comprobarAncho();
+});
