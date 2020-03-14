cd7eed87857437f7c10c6da16af70a7a6022185d
sotela88 <j-ms-27@hotmail.com>
2020-03-14 14:24:07 +0100

    youtube

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-
 //   setInterval(function time1() {
 //        var today = new Date();
 //      time =
 //       today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-      
+
 //       console.log();
-//       return 
+//       return
 //   }, 1000);
-
-
 
 //////// main function --menu and "videos" injection--///////
 let video = document.getElementById("right");
-  let left_menu = document.getElementById("left");
-  let inner_nav = document.getElementById("inner_nav");
+let left_menu = document.getElementById("left");
+let inner_nav = document.getElementById("inner_nav");
 function pic() {
-  
-
-
-///////////// lateral manu //////
+  ///////////// lateral manu //////
   for (let i = 0; i < 20; i++) {
     left_menu.innerHTML += `<a href="#"><i class="fas fa-home"></i>Home</a>
 
@@ -42,8 +36,7 @@ function pic() {
  
 </ul>`;
 
-  
-////////////// "videos" /////////////
+  ////////////// "videos" /////////////
   for (let i = 1; i <= 50; i++) {
     video_content = document.createElement(`Div`);
     video_content.className += "video_tex";
@@ -56,12 +49,9 @@ function pic() {
     </div>`;
     video.appendChild(video_content);
     video_content.innerHTML = video_inner;
-        
   }
 }
 pic();
-
-
 
 //////////////// functions to change the background----- dark mode simulation/////////
 function color1() {
@@ -69,16 +59,15 @@ function color1() {
   document.getElementById("nav").style.background = "rgba(136, 132, 203)";
   document.getElementById("left").style.background = "rgba(136, 132, 203)";
   document.getElementById("menu").style.background = "rgba(136, 132, 203)";
-  
+
   for (let i = 0; i < video.getElementsByTagName("h5").length; i++) {
-    video.getElementsByTagName("h5")[i].style.color='black' ; 
+    video.getElementsByTagName("h5")[i].style.color = "black";
   }
 
   for (let i = 0; i < video.getElementsByTagName("span").length; i++) {
-    video.getElementsByTagName("span")[i].style.color='black' ;
+    video.getElementsByTagName("span")[i].style.color = "black";
   }
 }
-
 
 function color2() {
   document.getElementById("right").style.background = "rgb(23, 41, 57)";
@@ -91,38 +80,39 @@ function color2() {
 //   for (let i = 0; i < video.getElementsByTagName("img").length; i++) {
 //     video.getElementsByTagName("img")[1].setAttribute(
 //   "style", "border: 2px solid red ");
-    
+
 //   }
- 
- 
+
 // }
 
 ///////////////  Hamburger menu toggle   ////////////////////
 const contenedor_video = document.getElementById("toggle");
 const contenedor_menu = document.getElementById("menu");
 
-const pop_menu = document.getElementById("pop_menu");
 
-document.getElementById('boton-menu').addEventListener('click', () => {
-  contenedor_video.classList.toggle('active');
-  contenedor_menu.classList.toggle('active');
-});
 
-document.getElementById('user').addEventListener('click', () => {
-  pop_menu.classList.toggle('able');
- 
+document.getElementById("boton-menu").addEventListener("click", () => {
+  contenedor_video.classList.toggle("active");
+  contenedor_menu.classList.toggle("active");
 });
 
 const resize = () => {
-	if(window.innerWidth >= 800){
-		contenedor_video.classList.remove('active');
-	} else {
-		contenedor_video.classList.add('active');
-	}
-}
+  if (window.innerWidth >= 800) {
+    contenedor_video.classList.remove("active");
+  } else {
+    contenedor_video.classList.add("active");
+  }
+};
 
 resize();
 
-window.addEventListener('resize', () => {
-	resize();
+window.addEventListener("resize", () => {
+  resize();
+});
+
+////////////pop-menu///////////
+const pop_menu = document.getElementById("pop_menu");
+
+document.getElementById("user").addEventListener("click", () => {
+  pop_menu.classList.toggle("able");
 });
